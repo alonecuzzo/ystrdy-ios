aace6cd02cc30568e409726fd3e73148ecd1c582
Jabari Bell <jabari.bell@pxlflu.net>
2014-12-22 13:25:00 -0500

    nimble and quick submodules installed properly

---
+++ ystrdy/ystrdyTests/TestSpec.swift
@@ -0,0 +1,17 @@
+//
+//  TestSpec.swift
+//  ystrdy
+//
+//  Created by Jabari Bell on 12/22/14.
+//  Copyright (c) 2014 tobros. All rights reserved.
+//
+
+import Foundation
+import Nimble
+import Quick
+
+class TestSpec: QuickSpec {
+    override func spec() {
+        
+    }
+}
